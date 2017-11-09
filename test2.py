7ef63b32fa886d3ddef90659508816bbb4985816
HaoKuo <mailhao@163.com>
2017-11-09 13:12:47 +0800

    del

--- test2.py
+++ test2.py
@@ -17,10 +17,34 @@ import pandas as pd
 from case.tabio import *
 
 
-print "hello world"
+#print "hello world"
 #def _cmd_target(args):
 #    regions = tabio.read_auto(args.interval)
 
+class Date(object):
+    day = 0
+    month = 0 
+    year = 0
+    def __init__(self, day=0, month=0, year=0):
+        self.day = 0
+        self.month = month
+        self.year = year
+
+    @classmethod
+    def from_string(cls, date_as_string):
+        day, month, year = map(int, date_as_string.split('-'))
+        date1 = cls(day, month, year)
+        return date1
 
 
+def test(arg):
+    print "Hello "+str(arg)
 
+
+def main():
+    fire.Fire(test)
+    date2 = Date.from_string('11-09-2012')
+    print date2
+
+if __name__ == "__main__":
+    main()
